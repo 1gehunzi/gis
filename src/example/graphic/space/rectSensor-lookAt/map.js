ae56d1993b28288ec6b4a1a04bac5a72bf921683
xiyuanyuan <xiyuanyan@meituan.com>
2022-10-18 22:26:50 +0800

    test

--- src/example/graphic/space/rectSensor-lookAt/map.js
+++ src/example/graphic/space/rectSensor-lookAt/map.js
@@ -5,7 +5,7 @@ export let map // mars3d.Map三维地图对象
 // 需要覆盖config.json中地图属性参数（当前示例框架中自动处理合并）
 export const mapOptions = {
   scene: {
-    center: { lat: 12.845055, lng: 112.931363, alt: 24286797, heading: 3, pitch: -90 },
+    center: { lat: 12.901283, lng: 112.928889, alt: 194340116.3, heading: 3, pitch: -89.9 },
     cameraController: {
       zoomFactor: 3.0,
       minimumZoomDistance: 1000,
@@ -61,9 +61,9 @@ function addGraphicLayer() {
   })
 
   const weixin = new mars3d.graphic.Satellite({
-    name: "GAOFEN 1",
-    tle1: "1 39150U 13018A   21180.50843864  .00000088  00000-0  19781-4 0  9997",
-    tle2: "2 39150  97.8300 252.9072 0018449 344.7422  15.3253 14.76581022440650",
+    name: "BEIDOU 3 (GEO)",
+    tle1: "1 36287U 10001A   22289.93799832 -.00000280  00000+0  00000+0 0  9990",
+    tle2: "2 36287   1.7230  48.2117 0000593 265.8062 189.5156  1.00268387 46730",
     model: {
       url: "//data.mars3d.cn/gltf/mars/weixin2.gltf",
       scale: 1,
@@ -104,9 +104,9 @@ function addGraphicLayer() {
 
   // 目标卫星
   const winxinMB = new mars3d.graphic.Satellite({
-    name: "COSMOS 2251 DEB",
-    tle1: "1 33916U 93036DV  21197.38574736  .00000034  00000-0  48020-4 0  9991",
-    tle2: "2 33916  74.0517 160.4563 0330253 328.4241 153.7022 13.66391564618811",
+    name: "BEIDOU 15 (MEO)",
+    tle1: "1 38775U 12050B   22289.62345197  .00000032  00000+0  00000+0 0  9991",
+    tle2: "2 38775  55.4724 107.6074 0011310 333.9961 355.7846  1.86231202 68719",
     model: {
       url: "//data.mars3d.cn/gltf/mars/weixin.gltf",
       scale: 1,
@@ -143,27 +143,70 @@ function addGraphicLayer() {
       width: 1
     }
   })
+
   graphicLayer.addGraphic(winxinMB)
 
-  // 四棱椎体
-  const rectSensor = new mars3d.graphic.RectSensor({
-    position: new Cesium.CallbackProperty(function (time) {
-      return weixin.position
-    }, false),
-    style: {
-      angle1: 10,
-      angle2: 10,
-      color: "rgba(0,255,0,0.4)",
+  const winxinMB2 = new mars3d.graphic.Satellite({
+    name: "BEIDOU 10 (IGSO)",
+    tle1: "1 37948U 11073A   22289.48464623 -.00000127  00000+0  00000+0 0  9992",
+    tle2: "2 43539  55.0895 174.6574 0051565 226.9118  89.5096  1.00273436 15721",
+    model: {
+      url: "//data.mars3d.cn/gltf/mars/weixin.gltf",
+      scale: 1,
+      minimumPixelSize: 90,
+      autoHeading: true,
+      show: true
+    },
+    label: {
+      color: "#ffffff",
+      opacity: 1,
+      font_family: "楷体",
+      font_size: 30,
       outline: true,
-      topShow: true,
-      topSteps: 2,
-      rayEllipsoid: true // 与地球求交
+      outlineColor: "#000000",
+      outlineWidth: 3,
+      background: true,
+      backgroundColor: "#000000",
+      backgroundOpacity: 0.5,
+      font_weight: "normal",
+      font_style: "normal",
+      pixelOffsetX: 0,
+      pixelOffsetY: -20,
+      scaleByDistance: true,
+      scaleByDistance_far: 10000000,
+      scaleByDistance_farValue: 0.4,
+      scaleByDistance_near: 100000,
+      scaleByDistance_nearValue: 1,
+      show: true
+    },
+    path: {
+      show: true,
+      color: "#00ff00",
+      opacity: 0.5,
+      width: 1
     }
   })
-  graphicLayer.addGraphic(rectSensor)
 
-  // 追踪目标
-  rectSensor.lookAt = new Cesium.CallbackProperty(function (time) {
-    return winxinMB.position
-  }, false)
+  graphicLayer.addGraphic(winxinMB2)
+  // 四棱椎体
+  // const rectSensor = new mars3d.graphic.RectSensor({
+  //   position: new Cesium.CallbackProperty(function (time) {
+  //     return weixin.position
+  //   }, false),
+  //   style: {
+  //     angle1: 10,
+  //     angle2: 10,
+  //     color: "rgba(0,255,0,0.4)",
+  //     outline: true,
+  //     topShow: true,
+  //     topSteps: 2,
+  //     rayEllipsoid: true // 与地球求交
+  //   }
+  // })
+  // graphicLayer.addGraphic(rectSensor)
+
+  // // 追踪目标
+  // rectSensor.lookAt = new Cesium.CallbackProperty(function (time) {
+  //   return winxinMB.position
+  // }, false)
 }
